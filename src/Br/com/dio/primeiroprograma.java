1328e3fc4808d8845172d6423a76a8304dbd2ffe
fabiocastrosprp <fabiocastrosprp@gmail.com>
2023-01-17 22:00:52 -0300

    first commit

---
+++ src/Br/com/dio/primeiroprograma.java
@@ -0,0 +1,16 @@
+package Br.com.dio;
+
+import Br.com.dio.model.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /* int a = 5;
+        int b = 3;
+        System.out.println("Hello mundo!" + (a+b));*/
+    }
+
+}
+
